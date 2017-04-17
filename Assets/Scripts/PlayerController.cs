45a95e0f3165fba4033413df210720124dccded8
Ryan Kortvelesy <Ryan@hntvlan554.00452.wlan.wireless-pennnet.upenn.edu>
2017-04-17 12:28:43 -0400

    Switched to BulletPhysics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,16 +3,14 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public GameObject player;
+	public GameObject player; // unused
 	public GameObject cam;
 	public GameObject ground;
-	public GameObject ropeprefab;
-	private GameObject rope;
 	private Rigidbody playerbody;
 
 	public float movespeed;
 	public float jumpheight;
-	public float jumpfloatforce;
+	public float jumpfloatforce; // For jetpack, edit this value
 
 	private Vector3 forward;
 	private Vector3 right;
@@ -41,13 +39,11 @@ public class PlayerController : MonoBehaviour {
 		touching = Physics.Raycast(transform.position,Vector3.down, out hit, 0.501f);
 
 		// Movement
-		if (touching) {
-			playerbody.AddForce (movement * movespeed - new Vector3 (playerbody.velocity.x, 0f, playerbody.velocity.z), ForceMode.VelocityChange);
-		}
+
+		playerbody.AddForce (movement * movespeed - new Vector3 (playerbody.velocity.x, 0f, playerbody.velocity.z), ForceMode.VelocityChange);
 		if (swinging) {
 			if (fire == 1) {
-				rope.SetActive (false);
-				Destroy (rope);
+				// Delete Rope
 				swinging = false;
 			}
 			transform.rotation = Quaternion.identity;
@@ -57,10 +53,8 @@ public class PlayerController : MonoBehaviour {
 			if (fire == 1) {
 				Physics.Raycast (cam.transform.position, (cam.transform.rotation * Vector3.forward), out hit, 20);
 				float distance = Vector3.Distance (player.transform.position, hit.point);
-				if (distance < 10.0f) {
-					CreateRope (hit.point, distance);
-					swinging = true;
-				}
+				// Fire rope
+				swinging = true;
 			}
 		}
 		if (jump != 0) {
@@ -71,16 +65,11 @@ public class PlayerController : MonoBehaviour {
 		}	
 	}
 
-	void CreateRope(Vector3 hitpoint, float distance) {
-		rope = Instantiate (ropeprefab);
-		GameObject ropeend = rope.transform.Find("Obi Handle").gameObject;
-		ropeend.transform.position = hitpoint;
-		GameObject solver = rope.transform.Find("Solver").gameObject;
-		solver.GetComponent<Obi.ObiColliderGroup>().colliders[0] = player.GetComponent<Collider>();
-		solver.GetComponent<Obi.ObiColliderGroup>().colliders[1] = ground.GetComponent<Collider>();
-		GameObject ropeobj = rope.transform.Find("Rope").gameObject;
-		ropeobj.GetComponent<Obi.ObiPinConstraints>().pinBodies[0] = player.GetComponent<Collider>();
-		ropeobj.GetComponent<Obi.ObiDistanceConstraints> ().stretchingScale = distance / 6f;
-	}
-
 }
+
+
+// Get public class variable:
+// objectinstance.GetComponent<scriptname>().variablename
+
+// Get child gameobject:
+// GameObject child = parent.transform.Find("childname").gameObject;
